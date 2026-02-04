a875fc06d8d9d49a93f7e5155da8af0c9c37c45a
sycophantic0 <sorteddrops@gmail.com>
2026-02-04 22:36:58 +0000

    javascript and css edits

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ let textDisplay = document.getElementById("textDisplay");
 let timeValue = document.getElementById("timeValue");
 let accValue = document.getElementById("accValue");
 let restartButton = document.getElementById("restart");
+
+let timeLeft = TimeLimit;
+let timeElapsed = 0;
+let total_errors = 0;
+let errors = 0;
+let accuracy = 0;
+let characterTyped = 0;
+let current_quote = "";
+let quoteNo = 0;
+let timer = null;
+
+
